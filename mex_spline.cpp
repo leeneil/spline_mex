e690c86bd23a5dcf02b87b9a3ca81ec94647e312
Po-Nan Li <lishantreasure@gmail.com>
2016-02-09 20:31:20 -0800

    first stable release

--- mex_spline.cpp
+++ mex_spline.cpp
@@ -4,9 +4,8 @@
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	double *yy, *YY;
-	double *xx, *XX;
-	double *y, *Y;
-	double *x, *X;
+	double *XX;
+	double *X, *Y;
 	
 	int m1, n1;
 	int m2, n2;
@@ -29,29 +28,16 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	int l3 = m3 * n3;
 
 	/* initialize arrays */
-	x  = new double[l1];
-	y  = new double[l1];
-	yy = new double[l3];
-	// YY = new double[l3];
-	// x = new int[sz1];
-
-
-	for (int i = 0; i < l1; i++) {
-		x[i] = X[i];
-		y[i] = Y[i];
-	}
-
+	yy = new double[l3]; // output buffer
+	
 	/* create output arguments */
 	plhs[0] = mxCreateDoubleMatrix(1, l3, mxREAL);
 	YY = mxGetPr(plhs[0]);
 
 	/* call core function */
-	spline(yy, x, y, XX, l1, l3);
+	spline(yy, X, Y, XX, l1, l3);
 
 	for(int i = 0; i < l3; i++) {
-		// YY[i] = 0;
 		YY[i] = yy[i];
 	}
-
-
 }
